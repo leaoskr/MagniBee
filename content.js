bfd110eaec0456e5297ba37055af85750e8d3dd4
施小芊 <lea@1x-nat-vl932-172-30-216-204.wireless.umass.edu>
2022-02-06 00:49:01 -0500

    update

--- content.js
+++ content.js
@@ -1,5 +1,53 @@
+let x = 20;
+
 window.onload = () => {
-    document.body.contentEditable = 'true'; 
-    document.designMode='on'; 
-    void 0
+   const button = document.createElement('button');
+   button.id = "searchButton";
+   button.textContent = "Magnify"
+
+   const searchbar = document.createElement('input');
+   searchbar.id = "searchbar";
+   
+   const slider = document.createElement('input');
+   slider.id = "fontsize";
+   slider.type = 'range';
+   slider.min = 1;
+   slider.max = 100;
+   slider.value = 20;
+
+   const temp = document.createElement('SPAN');
+   temp.id = "fontvalue";
+
+   document.querySelector("#test").append(searchbar);
+   document.querySelector("#test").append(button);
+   document.querySelector("#test").append(slider);
+   document.querySelector("#test").append(temp);
+
+   button.addEventListener('click', () => magnifyText());
+
+   temp.appendChild(document.createTextNode(slider.value));
+   
+   slider.oninput = function() {
+     temp.innerHTML = this.value;
+     x = this.value;
+   }
+
 }
+
+function magnifyText(){
+    const p = document.getElementById("p");
+    console.log("MagniBeeeeeee");
+    let input = document.getElementById("searchbar").value;
+    
+    x = x.toString();
+    y = "<p style='font-size:";
+    z = "pt;'>$&</p >";
+
+    if(input !== ""){
+            let regExp = new RegExp( input , "gi")
+            p.innerHTML = (p.textContent).replace(regExp, y+x+z);
+    }
+}
+
+
+
--- slideValue.js
+++ slideValue.js
@@ -3,6 +3,17 @@ var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
+
+
 slider.oninput = function() {
   output.innerHTML = this.value;
+  console.log(output);
+  localStorage.setItem("key", output);
 }
+
+
+// let temp = this.value
+// localStorage.setItem("key", temp);
+
+//export const getTemp = () =>temp;
+
